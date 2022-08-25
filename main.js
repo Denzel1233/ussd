3df7995cea63c5a2e3570df37815d0e80c2c2d53
Denzel <williamdenzel12@gmail.com>
2022-08-25 12:49:47 -0700

    sum
    apple

---
+++ main.js
@@ -0,0 +1,14 @@
+let opr=prompt("welcome to *495 banking\n 1*Quick banking\n 2*open an account\n 3* loans\n4*select bank\n5*pay attitude ")
+// let num2=parseInt (prompt("Enternm")
+
+// introduction to Conditional statement
+
+
+if(opr===1){}
+//if 7 go back to prompt
+    let QuickBank=parseInt(prompt("welcome to *894 banking\n 1*Transfer money\n 2* Airtime\n 3*pay AEDC\n 4* Enquiry services\n 5* Data\n 6* pay bills\n 7* Go back "))
+
+    if( QuickBank===2){}
+       alert("THANK YOU")
+       alert("Youcan trust us")
+    
